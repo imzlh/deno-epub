c9fae182fc49cfd0f718e21365df4092b89983ac
imzlh <2131601562@qq.com>
2025-02-11 18:41:20 +0800

    test: fix dir `test` not always exists

--- tests/download.ts
+++ tests/download.ts
@@ -1,4 +1,6 @@
 // import epubgen from "https://esm.sh/epub-gen-memory@1.0.9/sabstub?target=node&deps=domhandler,domutils,htmlparser2";
+// @ts-ignore
+import { ensureDir } from "fs";
 import epubgen from "../mod.ts";
 
 import { contentAlice, optionsAlice } from "./aliceData.ts"; // modified copy of https://github.com/cpiber/epub-gen-memory/blob/master/tests/aliceData.ts
@@ -14,6 +16,7 @@ class Download {
     try {
       const filename = `alice_${this.uuid}.epub`;
       const content = await epubgen(optionsAlice, contentAlice);
+      ensureDir('temp');
       await Deno.writeFile(
         `temp/${filename}`,
         content,
