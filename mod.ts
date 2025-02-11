84d08cd110a6e2c7c11de8cbb92e5703bf2cba9b
imzlh <2131601562@qq.com>
2025-02-11 19:18:08 +0800

    add comment for main function

--- mod.ts
+++ mod.ts
@@ -3,11 +3,32 @@ import { Content, Options, optionsPredicate } from "./util/validate.ts";
 
 import { EPub } from "./epub.ts";
 
-const epub = (
+/**
+ * Generate an EPub file from the given options and content.
+ * @example
+ * ```ts
+ * epub(options, [
+ *   {
+ *     title: "Chapter 1",
+ *     content: "<p>Hello world!</p>",
+ *   },
+ * ]).then(
+ *   (content) => {
+ *     Deno.writeFileSync("book.epub", content);
+ *   },
+ *   (err) => console.error("Failed to generate Ebook because of ", err),
+ * );
+ * ```
+ * @param optionsOrTitle The options or title of the EPub.
+ * @param content The content of the EPub.
+ * @param args Additional arguments for the EPub generation.
+ * @returns Buffer of the generated EPub file.
+ */
+export default function (
   optionsOrTitle: Options | string,
   content: Content,
   ...args: (boolean | number)[]
-): Promise<Uint8Array> => {
+): Promise<Uint8Array> {
   ow(optionsOrTitle, ow.any(optionsPredicate, ow.string));
   const options = ow.isValid(optionsOrTitle, ow.string)
     ? { title: optionsOrTitle }
@@ -20,4 +41,3 @@ const epub = (
 
   return new EPub(options, content).genEpub();
 };
-export default epub;
